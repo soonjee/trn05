411e76f08a87a6af2553f83c6da30f6ca537d165
soonji <youinxxs@naver.com>
2022-11-10 12:08:28 +0900

    back up_2022.11.10

---
+++ sgc.js
@@ -0,0 +1,28 @@
+$(function () {
+    var TXT = ['HOME', '01', '02', '03', '04', 'portfolio', 'copyright']
+    $('.main').fullpage({
+        anchors: ['page01', 'page02', 'page03', 'page04', 'page05', 'footer'],
+        //navigation: true,
+        css3: false,
+        afterLoad: function (page, num) {
+            //애니메이션을 큐스텍에 담아서 딜레이 시킴...
+            setTimeout(function () {
+                $('.section').eq(num - 1).addClass('on').siblings().removeClass('on');
+            });
+            $('.navBar li').eq(num - 1).addClass('on').siblings().removeClass('on');
+            $('.this_page').text(TXT[num - 1]);
+
+            //부정연산자 사용
+            num !== 1
+                ? $('.Header').addClass('on')
+                : $('.Header').removeClass('on')
+        },
+    });
+
+
+    $('.basicSlider').slick({
+        arrows: false,
+        asNavFor: '.basicSlider',
+    });
+
+})
